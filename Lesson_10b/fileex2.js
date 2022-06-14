c687b6f5db421cafec9c1dabde306f4091485727
trishaMagical <trisha.hazra@madgicaltechdom.com>
2022-06-14 18:19:05 +0530

    error resolved

--- Lesson_10b/fileex2.js
+++ Lesson_10b/fileex2.js
@@ -1,8 +1,9 @@
+const { fail } = require("assert");
 let fs = require("fs");
 let readline =require ("readline-sync");
 let fname = "data.json";
 let courseData = {
-    course: "Node.js",
+    course: "React.js",
     students:[
         {name : "Trisha", age: 25},
         {name : "Chitra", age: 35},
@@ -11,19 +12,19 @@ let courseData = {
 };
 
 function writeJson(){
-    let str = JSON.stringify(courseData);
-    fs.watchFile(fname,str,function(err){
-        if (err) console.log(err);
-    });
+    let str= JSON.stringify(courseData)
+    fs.writeFile(fname,str, function(err){
+        if(err) console.log(err);
+    })
 }
 
-function updateJSON(){
-    let name = readline.question("Enter name of the student:");
-    let age = readline.question("Enter name of the age:");
-     let newStudent = {name: name , age: age};
-     fs.readFile(fname,"utf8", function(err, data){
-        if (err) console.log(err);
-        else {
+function appendJson(){
+    let name = readline.question("Enter the name");
+    let age = readline.question("Enter the age");
+    let newStudent ={name:name, age: age};
+    fs.readFile(fname, "utf8",function(err,data){
+        if(err) console.log(err);
+        else{
             let obj = JSON.parse(data);
             obj.students.push(newStudent);
             let data1 = JSON.stringify(obj);
@@ -32,28 +33,26 @@ function updateJSON(){
                 else console.log("Data Updated");
             })
         }
-
-     });
-}
- function readJson(){
-    fs.readFile(fname,"utf8",function(err,data){
-        if (err) console.log(err);
-        else{
-            let obj = JSON.parse(data);
-            console.log(obj);
-        }
     });
- }
-  let option = readline.question("Enter option 1: write 2:Update json-");
-  switch(option) {
-      case "1":
-          writeJson();
-          break;
-      case "2":
-          updateJSON();
-          break;
-      case "3" : readJson();
-        break;
-        
 
-  }
+}
+
+function readJson(){
+  fs.readFile(fname,"utf8", function(err,data){
+    if(err) console.log(err);
+    else{
+        let obj = JSON.parse(data);
+        console.log(obj);
+    }
+  });  
+}
+
+let option = readline.question("nter option 1: write 2:Update json-");
+switch(option){
+    case "1" : writeJson();
+    break;
+    case "2": appendJson();
+    break;
+    case "3": readJson();
+    break;
+}
